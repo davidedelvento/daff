6182187a084b62258de266630c6c5ffd5cf2fed0
Davide Del Vento <davide.del.vento@gmail>
2016-03-25 11:14:35 -0600

    TDD: the desired behavior, when threashold=0

--- harness/EqualityTest.hx
+++ harness/EqualityTest.hx
@@ -9,6 +9,7 @@ class EqualityTest extends haxe.unit.TestCase {
     var data4 : Array<Array<Dynamic>>;
     var fp_data : Array<Array<Dynamic>>;
     var fp_data_1pc : Array<Array<Dynamic>>;
+    var fp_data_exp : Array<Array<Dynamic>>;
 
     override public function setup() {
         data1 = [['Country','Capital'],
@@ -31,9 +32,23 @@ class EqualityTest extends haxe.unit.TestCase {
                        ['82.456',  '149',  '0.553', '130.106', '18.391']];
         fp_data_1pc = [['Time',    'Steps','Avg',   'Speed',   'I/O time'],
                        ['82.282',  '149',  '0.553', '130.2',   '18.317']];
+        fp_data_exp = [['Time',    'Steps','Avg',   'Speed',   'I/O time'],
+                       ['8.245600e+01',  '149',  '5.530000e-01', '1.301060e+02', '1.839100e+01']];
     }
 
-    public function testFloatingPoints(){
+    public function testFloatingPoints_exp(){
+        var table1 = Native.table(fp_data);
+        var table2 = Native.table(fp_data_exp);
+        var flags = new coopy.CompareFlags();
+        flags.unchanged_context = 0;
+        var o = coopy.Coopy.diff(table1,table2,flags);
+        assertEquals(2,o.height);
+        flags.fp_threshold = 0.0;
+        o = coopy.Coopy.diff(table1,table2,flags);
+        assertEquals(1,o.height);
+    }
+
+    public function testFloatingPoints_1pc(){
         var table1 = Native.table(fp_data);
         var table2 = Native.table(fp_data_1pc);
         var flags = new coopy.CompareFlags();
